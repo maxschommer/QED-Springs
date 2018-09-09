1db54e944799e5eb625ee8483b9794524d28e84a
Maximilian Schommer <maxschommer@gmail.com>
2018-09-09 14:16:57 -0400

    Undoing a stash

--- 4_11 working/moveOneServo.m
+++ 4_11 working/moveOneServo.m
@@ -1,5 +1,16 @@
 function moveOneServo(K, States)
+<<<<<<< Updated upstream
 
+=======
+    numMasses = 5;
+    for targetNum = 0:1:(2^(numMasses-1)-1)     
+        biV = de2bi(targetNum, numMasses);
+        biV(biV==0)=-1;
+        statesM(targetNum+1, :) = cat(2, zeros(1, numMasses), biV);
+    end
+    statesM(4,:)
+    
+>>>>>>> Stashed changes
     if exist('a') ~= 1
         a = arduino;
         s = servo(a,'D4');
@@ -12,7 +23,7 @@ function moveOneServo(K, States)
 
     A = K(1:length(K)/2);
     F = K(length(K)/2+1:end);
-    
+    [T,X,drive,~,idx] = runOde(K,statesM(4,:));
     tRange = linspace(0,10,500);
     driving = @(t) dot(sin(t*F),A)/10 +.5;
 
@@ -23,9 +34,12 @@ function moveOneServo(K, States)
     t0 = clock;
     t = 0;
 
-    while t < 2*pi
+    while t < 15
        t = etime(clock , t0);
        writePosition(s,driving(t))
+       if t > T(idx)
+           poop
+       end
     end
 
     pause(3)
